88a696bed6d555253ce8495e385b2840404d14f6
Joel Hernandez <lifenautjoe@gmail.com>
2018-04-06 21:38:27 +0200

    ✨(website) add code highlighting and mobile background

--- website/src/index.js
+++ website/src/index.js
@@ -1,9 +1,10 @@
 
 require('normalize.css/normalize.css');
 require('./styles/index.scss');
+require('./assets/scripts/highlight.pack.exec');
 
 import Droppable from 'droppable';
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log(Droppable);
+    window['hljs'].initHighlightingOnLoad();
 });
--- website/webpack.prod.js
+++ website/webpack.prod.js
@@ -18,6 +18,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.exec\.js$/,
+                use: [ 'script-loader' ]
+            },
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
