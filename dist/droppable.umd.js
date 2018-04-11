ef607c77d5de7d16912ba1a3ff4c9f4e7f6fcb4a
Joel Hernandez <lifenautjoe@gmail.com>
2018-04-11 22:02:07 +0200

    📦 add 2.1.5 package dist files

--- dist/droppable.umd.js
+++ dist/droppable.umd.js
@@ -132,7 +132,8 @@ var Droppable = /** @class */ (function () {
         });
     };
     Droppable.prototype.onElementFocusOut = function () {
-        this.elementKeyDownEventRemover();
+        if (this.elementKeyDownEventRemover)
+            this.elementKeyDownEventRemover();
     };
     Droppable.prototype.onVirtualInputElementChange = function (e) {
         this.onDroppableElementChange(e);
